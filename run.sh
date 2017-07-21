a9097572665ffb095647384cd3a6a1c1083230cc
fdns <yayofelipe@gmail.com>
2017-07-21 13:16:12 -0400

    Refactoring run script

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-#set -e
-(cd tests/prometheus && docker-compose build && docker-compose up -d)
-pid="$(ps -aux | grep prometheus | grep -v grep | grep -v main.py | awk '{ print $2}')"
-echo "Waiting 1 minute for setup"
-trap 'trap - INT' INT
-sleep 10s
-python loadgenerator/src/main.py --prometheus --domain "$pid" 'prometheus_prometheus_volume' 60 1000
-(cd tests/prometheus && docker-compose down -v)
+
+run_benchmark() {
+    (cd $1 && docker-compose build && docker-compose up -d)
+    trap "(cd $1 && docker-compose down -v); exit" INT
+    pid="$(ps -aux | grep $4 | grep -v grep | grep -v main.py | awk '{ print $2}')"
+    echo "Waiting 1 minute for setup"
+    sleep 10s
+    python loadgenerator/src/main.py --debug --$2 --$3 "$pid" "$5" 60 1000 > $6
+    (cd $1 && docker-compose down -v)
+    trap - INT
+}
+
+run_benchmark tests/prometheus prometheus domain prometheus prometheus_prometheus_volume out/prometheus_domain_1.out
+run_benchmark 'tests/prometheus' 'prometheus' 'mask' 'prometheus' 'prometheus_prometheus_volume' out/prometheus_domain_1.out
+run_benchmark 'tests/prometheus' 'prometheus' 'length' 'prometheus' 'prometheus_prometheus_volume' out/prometheus_domain_1.out
