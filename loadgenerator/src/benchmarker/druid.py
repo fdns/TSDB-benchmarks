098411ee205ad399cf7f87ef16b0e4174fb8bc4e
fdns <yayofelipe@gmail.com>
2017-07-24 17:51:59 -0400

    Primeras pruebas con base de datos Druid

---
+++ loadgenerator/src/benchmarker/druid.py
@@ -0,0 +1,39 @@
+import datetime
+import json
+import urllib, urllib2
+
+from load_iterators import generate_random_domain_iterator
+
+it = generate_random_domain_iterator(5, 100)
+def get_rand():
+    return {
+        'timestamp': datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        'domain': next(it)
+    }
+
+if __name__ == '__main__':
+    url = 'http://localhost:8200/v1/post/domains'
+
+    request = urllib2.Request(url,
+                              headers={'Content-Type': 'application/json'},
+                              data='\n'.join([json.dumps(get_rand()) for _ in range(500)]))
+    opener = urllib2.build_opener()
+    print('sending')
+    while True:
+        print(opener.open(request).read())
+        import time
+        time.sleep(1)
+        break
+    print('sended')
+    import os
+    os._exit(0)
+    from pydruid.client import *
+    from pydruid.utils.aggregators import doublesum
+    query = PyDruid("http://localhost:8082", 'druid/v2')
+    ts = query.timeseries(
+        datasource='domains',
+        granularity='minute',
+        intervals='2017-07-24/p4w',
+        aggregations={'count': doublesum('count')},
+    )
+    print(ts[:])
