f3901208633ea0e06bffa1a0fb771e5fc9518bfe
fdns <yayofelipe@gmail.com>
2017-07-27 16:58:38 -0400

    Abilitando todos los benchmarks

--- run.sh
+++ run.sh
@@ -11,9 +11,9 @@ run_benchmark() {
     trap - INT
 }
 
-#run_benchmark dockerfiles/prometheus prometheus domain /bin/prometheus prometheus_prometheus_volume out/prometheus_domain_1.out
-#run_benchmark dockerfiles/prometheus prometheus mask /bin/prometheus prometheus_prometheus_volume out/prometheus_mask_1.out
-#run_benchmark dockerfiles/prometheus prometheus length /bin/prometheus prometheus_prometheus_volume out/prometheus_length_1.out
+run_benchmark dockerfiles/prometheus prometheus domain /bin/prometheus prometheus_prometheus_volume out/prometheus_domain_1.out
+run_benchmark dockerfiles/prometheus prometheus mask /bin/prometheus prometheus_prometheus_volume out/prometheus_mask_1.out
+run_benchmark dockerfiles/prometheus prometheus length /bin/prometheus prometheus_prometheus_volume out/prometheus_length_1.out
 run_benchmark dockerfiles/druid druid domain "io.druid.cli.Main" druid_druid_volume out/druid_domain_1.out
-#run_benchmark dockerfiles/druid druid mask "io.druid.cli.Main" druid_druid_volume out/druid_mask_1.out
-#run_benchmark dockerfiles/druid druid length "io.druid.cli.Main" druid_druid_volume out/druid_length_1.out
+run_benchmark dockerfiles/druid druid mask "io.druid.cli.Main" druid_druid_volume out/druid_mask_1.out
+run_benchmark dockerfiles/druid druid length "io.druid.cli.Main" druid_druid_volume out/druid_length_1.out
