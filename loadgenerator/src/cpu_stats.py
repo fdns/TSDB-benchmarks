1e5173908f0e710c35991d0f622fb55a602411be
Felipe Espinoza <fdns@users.noreply.github.com>
2017-07-28 11:43:17 -0400

    Update cpu_stats.py

--- loadgenerator/src/cpu_stats.py
+++ loadgenerator/src/cpu_stats.py
@@ -7,36 +7,26 @@ logger = logging.getLogger(__name__)
 
 
 def fetch_proc_stats(pids):
-    logger.debug('fetch_proc_stats')
-    cpu, memory = 0,0
+    memory = 0
     for pid in pids.split('\n'):
         try:
             result = check_output("ps --no-headers -o rss {}".format(pid), shell=True)
-            logger.debug(result)
         except Exception as e:
             logger.exception(e)
-            import time
-            time.sleep(60)
             import os
             os._exit(0)
         memory += int(result.strip())
-    logger.debug('fetch_proc_stats RETURN')
-    return (fetch_proc_stats_old(pids), memory)
+    return (fetch_cpu_stats(pids), memory)
 
-def fetch_proc_stats_old(pids):
-    """Deprecated, use the /proc file"""
-    cpu, memory = 0,0
-    logger.debug('fetch_proc_stats_old')
+def fetch_cpu_stats(pids):
+    cpu = 0
     for pid in pids.split('\n'):
-        logger.debug('Opening')
         with open('/proc/{}/stat'.format(pid)) as stat_file:
             line = stat_file.readline()
             logger.debug(line)
             data = line.split(' ')
             cpu += int(data[13]) + int(data[14])
-    logger.debug('fetch_proc_stats_old RETURN')
     return cpu
-    return (cpu, memory)
 
 mutex = Lock()
 def fetch_docker_disk_usuage(volume):
