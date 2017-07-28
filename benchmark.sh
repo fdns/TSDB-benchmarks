684800cfac85546ba3b3b2bd4ae416cc4dd48925
Felipe Espinoza <fdns@users.noreply.github.com>
2017-07-28 13:24:01 -0400

    Adding travis support (#2)

---
+++ benchmark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+(cd $1 && docker-compose build && docker-compose up -d)
+trap "(cd $1 && docker-compose down -v); exit" INT
+echo "Waiting 60 seconds for setup"
+sleep 60s
+pid="$(ps -aux | grep $4 | grep -v grep | grep -v benchmark.sh | grep -v main.py | awk '{ print $2}')"
+python loadgenerator/src/main.py --debug --$2 --$3 "$pid" "$5" 60 1000 > $6
+(cd $1 && docker-compose down -v)
+trap - INT
--- loadgenerator/src/cpu_stats.py
+++ loadgenerator/src/cpu_stats.py
@@ -1,44 +1,41 @@
 from subprocess import check_output
 from threading import Lock
 import re
+import os
 
 import logging
 logger = logging.getLogger(__name__)
 
 
 def fetch_proc_stats(pids):
-    cpu, memory = 0,0
+    memory = 0
     for pid in pids.split('\n'):
         try:
-            result = check_output("ps --no-headers -o %cpu,rss {}".format(pid), shell=True)
+            result = check_output("ps --no-headers -o rss {}".format(pid), shell=True)
+            memory += int(result.strip())
         except Exception as e:
             logger.exception(e)
-            import time
-            time.sleep(60)
-            import os
-            os._exit(0)
-        result = result.strip().split(' ')
-        cpu += float(result[0])
-        memory += int(result[1])
-    return (fetch_proc_stats_old(pids), memory)
+            os._exit(1)
+    return (fetch_cpu_stats(pids), memory)
 
-def fetch_proc_stats_old(pids):
-    """Deprecated, use the /proc file"""
-    cpu, memory = 0,0
+def fetch_cpu_stats(pids):
+    cpu = 0
     for pid in pids.split('\n'):
-        with open('/proc/{}/stat'.format(pid)) as stat_file:
-            line = stat_file.readline()
-            data = line.split(' ')
-            cpu += int(data[13]) + int(data[14])
-            #cpu, memory = (cpu + cpu_time, memory + int(data[23]))
+        try:
+            with open('/proc/{}/stat'.format(pid)) as stat_file:
+                line = stat_file.readline()
+                data = line.split(' ')
+                cpu += int(data[13]) + int(data[14])
+        except Exception as e:
+            logger.exception(e)
+            os._exit(1)
     return cpu
-    return (cpu, memory)
 
 mutex = Lock()
 def fetch_docker_disk_usuage(volume):
     regex = re.compile(r'(\d+(?:\.\d+)?)\s*([kmgtp]?[Bb])', re.IGNORECASE)
     order = ['b', 'kb', 'mb', 'gb', 'tb']
     with mutex:
-        size = check_output("docker system df -v | grep {} | awk '{{ print $3 }}'".format(volume), shell=True)
+        size = check_output("docker system df -v | grep {} | awk '{{ print $3$4 }}'".format(volume), shell=True)
         value, unit = regex.findall(size)[0]
         return float(value) * (1024**order.index(unit.lower()))
