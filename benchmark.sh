97f62a6d409d9d9a748ec6aec5452b8755a295ea
Felipe Espinoza <yayofelipe@gmail.com>
2017-08-10 16:32:17 -0400

    Moving tests back to 10 minutes

--- benchmark.sh
+++ benchmark.sh
@@ -8,6 +8,6 @@ trap "(cd $1 && docker-compose down -v); exit" INT
 echo "Waiting 60 seconds for setup"
 sleep 60s
 pid="$(ps -aux | egrep $4 | grep -v grep | grep -v benchmark.sh | grep -v main.py | awk '{ print $2}')"
-python benchmarker/src/main.py --$2 --$3 "$pid" "$5" 60 3000 > $6
+python benchmarker/src/main.py --$2 --$3 "$pid" "$5" 600 3000 > $6
 (cd $1 && docker-compose down -v)
 trap - INT
