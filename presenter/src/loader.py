d5fc2e7b0622a79680ae6ef2aecc857e4e16fab5
Felipe Espinoza <yayofelipe@gmail.com>
2017-08-18 11:08:04 -0300

    Agregando un timeout mas grande y elminando datos nulos del presentador

--- presenter/src/loader.py
+++ presenter/src/loader.py
@@ -8,6 +8,7 @@ def load(filename):
             try:
                 data = json.load(file)
                 check_data_validity(data, file)
+                clear_invalid_points(data)
                 return data
             except ValueError:
                 logger.error('Error parsing json data from file {}'.format(file))
@@ -20,3 +21,6 @@ def check_data_validity(data, file):
     data = data['validation']
     if data[0] != int(data[1]):
         logger.warning('The data is not falidated ({} != {}, {} difference) for {}'.format(data[0], data[1], data[0] - data[1], file))
+
+def clear_invalid_points(data):
+    data['query'] = [x for x in data['query'] if x[1] >= 0]
